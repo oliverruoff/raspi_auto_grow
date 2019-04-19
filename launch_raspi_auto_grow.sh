6aa5762e6914a0f3a3f8f40a86bb013c6c388790
Oliver Ruoff <oliver93ruoff@web.de>
2019-04-19 13:29:28 +0200

    switch from module to script

--- launch_raspi_auto_grow.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo python3 -m /home/pi/raspi_auto_grow
--- raspi_auto_grow.py
+++ raspi_auto_grow.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as gp
 import time
+from datetime import datetime
 from notify_run import Notify
 
 # Pins used for water pump and humidty sensor
 PUMP_PIN = 20
 HUMID_SENS_PIN = 21
 
+# Check interval time
+CHECK_INTERVAL_IN_SECONDS = 20
 # How long the water pump will pump in one run
 PUMP_TIME = 5
 
@@ -38,3 +41,17 @@ def run_pump():
     # Have to set pin to "IN", since taking away 3,3V (from raspi)
     # isn't enough to turn off the pump
     gp.setup(PUMP_PIN, gp.IN)
+
+
+if __name__ == '__main__':
+    while(True):
+        if soil_is_dry():
+            print(datetime.now(),
+                  'Soil seems to be dry, watering the plant now.')
+            notify.send(str(datetime.now().strftime(
+                "%d.%m.%Y, %H:%M:%S")) + ' > Watering :)')
+            run_pump()
+        else:
+            print(datetime.now(),
+                  'Soil seems to be humid, won\'t water the plant.')
+        time.sleep(CHECK_INTERVAL_IN_SECONDS)
