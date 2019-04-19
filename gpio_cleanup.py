6c6cea6f822d0acbb994920299063736061ceb21
Oliver Ruoff <oliver93ruoff@web.de>
2019-04-19 13:42:18 +0200

    fix watering, added gpio cleanup script

---
+++ gpio_cleanup.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as gp
+
+# Pins used for water pump and humidty sensor
+PUMP_PIN = 20
+HUMID_SENS_PIN = 21
+
+# Setting up the pins
+gp.setmode(gp.BCM)
+gp.setup(PUMP_PIN, gp.OUT)
+gp.setup(HUMID_SENS_PIN, gp.IN)
+# cleaning up the pins
+gp.cleanup()
