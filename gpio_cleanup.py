6a20b008a316ff4e4236f8a256fd92977a077936
Oliver Ruoff <oliver93ruoff@web.de>
2019-04-30 00:15:04 +0200

    extended gpio cleanup

--- gpio_cleanup.py
+++ gpio_cleanup.py
@@ -5,10 +5,12 @@ import RPi.GPIO as gp
 # Pins used for water pump and humidty sensor
 PUMP_PIN = 18
 HUMID_SENS_PIN = 21
+HUMID_SENS_RELAY_PIN = 26
 
 # Setting up the pins
 gp.setmode(gp.BCM)
 gp.setup(PUMP_PIN, gp.OUT)
 gp.setup(HUMID_SENS_PIN, gp.IN)
+gp.setup(HUMID_SENS_RELAY_PIN, gp.OUT)
 # cleaning up the pins
 gp.cleanup()
